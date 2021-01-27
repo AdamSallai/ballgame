062f4936528f88e4657ad132989c9677adc20cc4
Adam Sallai <yadam1998@gmail.com>
2021-01-27 11:58:27 +0100

    add image imports to a js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {images} from './images/images'
 
 const App = () => {
   return (
     <div className="App">
+      <img src={images.wall} alt=""/>
     </div>
   );
 }
