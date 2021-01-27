bd112816cdc34a37ceb0a4ee51fa69e54c9f2393
Adam Sallai <yadam1998@gmail.com>
2021-01-27 16:50:18 +0100

    add api file and placeholder return map value

---
+++ src/api/mapApi.js
@@ -0,0 +1,12 @@
+export const getMap = () => {
+  return exampleMap;
+}
+
+const exampleMap = [
+  ["W","W","W","W","W","W","W"],
+  ["W","F","F","F","F","F","W"],
+  ["W","F","F","W","F","F","W"],
+  ["W","F","F","W","F","F","C"],
+  ["D","F","F","W","F","F","W"],
+  ["W","W","W","W","W","W","W"]
+]
